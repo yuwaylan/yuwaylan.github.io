12f342ffe6c6c1522a5ea8ca38ea7c621343923c
yuwaylan <0987431104a@gmail.com>
2020-11-25 20:43:43 +0800

    ip change

--- togit.bat
+++ togit.bat
@@ -1,7 +1,5 @@
-(
+D:
+cd  0987\Documents\GitHub\yuwaylan.github.io\
 git add . 
-ping 127.99.0.1 -n 1 -w 1000
 git commit -m "ip change" 
-ping 127.99.0.1 -n 1 -w 1000
 git push
-)| cd  D:\0987\Documents\GitHub\yuwaylan.github.io\
